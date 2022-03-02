a78f24ba4b9656ccc7d3900adfaff026f7f34058
tarsilaschorr <89028181+tarsilaschorr@users.noreply.github.com>
2022-03-02 17:53:23 -0400

    added the search engine

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import "./App.css";
+import Dictionary from "./Dictionary.js";
 
 function App() {
   return (
-    <div className="App">
+    <div className="container">
       <header className="App-header">Dictionary</header>
+      <Dictionary />
     </div>
   );
 }
---
+++ src/Dictionary.js
@@ -0,0 +1,20 @@
+import React, { useState } from "react";
+
+export default function Dictionary() {
+  const [keyword, setKeyword] = useState("");
+
+  function search(event) {
+    event.preventDefault();
+    alert(`Searching for ${keyword}`);
+  }
+  function handleKeywordChange(event) {
+    setKeyword(event.target.value);
+  }
+  return (
+    <div className="Dictionary">
+      <form onSubmit={search}>
+        <input type="search" onChange={handleKeywordChange} />
+      </form>
+    </div>
+  );
+}
