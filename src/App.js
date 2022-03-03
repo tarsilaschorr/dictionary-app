586e2fa9a64b65a6e9049c4574114ae6157309ea
tarsilaschorr <89028181+tarsilaschorr@users.noreply.github.com>
2022-03-03 11:58:52 -0400

    added the footer

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import "./App.css";
 import Dictionary from "./Dictionary.js";
+import Footer from "./Footer";
 
 function App() {
   return (
     <div className="container">
       <header className="App-header">Dictionary</header>
       <Dictionary />
+      <Footer />
     </div>
   );
 }
