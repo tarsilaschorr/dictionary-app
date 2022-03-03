535e527bf2b226e61fe121bc37970bac9faabd11
tarsilaschorr <89028181+tarsilaschorr@users.noreply.github.com>
2022-03-03 10:33:41 -0400

    API integration

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,10 +1,18 @@
 import React, { useState } from "react";
+import axios from "axios";
 
 export default function Dictionary() {
   const [keyword, setKeyword] = useState("");
 
+  function handleResponse(response) {
+    console.log(response.data[0]);
+  }
+
   function search(event) {
     event.preventDefault();
+
+    let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
+    axios.get(apiUrl).then(handleResponse);
     alert(`Searching for ${keyword}`);
   }
   function handleKeywordChange(event) {
